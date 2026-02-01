671cbe5f05cfd093a68fac09c0e6e8fa5a52689f
mazzon0 <danielemazzon2004@gmail.com>
2026-02-01 15:28:40 +0100

    updated setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,19 +3,24 @@
 set -euo pipefail
 
 # Load GCC and G++
+echo "Loading GCC..."
 module load gcc91
-module load g++91
 gcc () { gcc -9.1.0 "$@"; }
-g++ () { g++ -9.1.0 "$@"; }
-gcc --version
-g++ --version
+echo "Loaded GCC"
+echo ""
 
 # Compile
+echo "Compiling..."
 make clean
 make
+echo "Compilation finished"
+echo ""
 
 # Download graphs
+echo "Downloading graphs..."
 scripts/download.sh stanford
 scripts/download.sh google
+echo "Graphs downloaded"
+echo ""
 
 # TODO add other implementations to test
